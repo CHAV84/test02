c3cefbb9a22ce5d4f808ecd0d5baaf352af0209d
CHAV84 <137306972+CHAV84@users.noreply.github.com>
2025-07-24 11:57:54 +0200

    Create cr_test_math_utils.sql

---
+++ cr_test_math_utils.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE PACKAGE test_math_utils IS
+  --%suite(Math Utils Tests)
+  --%suitepath(math)
+
+  --%test(Square of 3 is 9)
+  PROCEDURE test_square_of_3;
+
+  --%test(Square of -4 is 16)
+  PROCEDURE test_square_of_minus_4;
+END;
+/
+
+CREATE OR REPLACE PACKAGE BODY test_math_utils IS
+
+  PROCEDURE test_square_of_3 IS
+    v_result NUMBER;
+  BEGIN
+    v_result := math_utils.square(3);
+    ut.expect(v_result).to_equal(9);
+  END;
+
+  PROCEDURE test_square_of_minus_4 IS
+    v_result NUMBER;
+  BEGIN
+    v_result := math_utils.square(-4);
+    ut.expect(v_result).to_equal(16);
+  END;
+
+END;
+/
